609c37c0a5ccf9be1af06745244f759d77310a03
Revolut CI <ci@revolut.com>
2020-11-10 14:02:39 +0000

    Push docs of build 677

---
+++ api/precache-manifest.12e662dfa4b53188cc95908969b8eac2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a854fe04650af905df26c25f4a54c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e41078a96eaf2ccdd683",
+    "url": "/static/css/main.d9e0d89c.chunk.css"
+  },
+  {
+    "revision": "44e7e27e93d704873d55",
+    "url": "/static/js/2.643e275a.chunk.js"
+  },
+  {
+    "revision": "8a615afa3741d9dad911101709fdc8aa",
+    "url": "/static/js/2.643e275a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4127299f9ca61f9b3be5",
+    "url": "/static/js/3.6b254fbb.chunk.js"
+  },
+  {
+    "revision": "e41078a96eaf2ccdd683",
+    "url": "/static/js/main.d0380852.chunk.js"
+  },
+  {
+    "revision": "4379d5f70ff4ec2495df",
+    "url": "/static/js/runtime-main.7172f322.js"
+  },
+  {
+    "revision": "577a807fdc91a1aef73aa73757f8252d",
+    "url": "/static/media/business.577a807f.yaml"
+  },
+  {
+    "revision": "685f20125b4f8632b57e6f4d42b5764f",
+    "url": "/static/media/merchant.685f2012.yaml"
+  },
+  {
+    "revision": "e911c013e3106c6432262ac936eb2802",
+    "url": "/static/media/open-banking.e911c013.yaml"
+  }
+]);
