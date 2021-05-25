82f5d22a659174296220418fda5d90ab2ea81361
Revolut CI <ci@revolut.com>
2021-05-25 18:18:05 +0000

    Push docs of build 1098

---
+++ api/precache-manifest.ca475370534c07715e97085472d2c0c4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2f9de4a420ac12794b09e2fe99954b",
+    "url": "/api-reference/index.html"
+  },
+  {
+    "revision": "d2ec037cb53c91ad06a9",
+    "url": "/api-reference/static/css/main.dbd200d2.chunk.css"
+  },
+  {
+    "revision": "f88b462ca9815fbcf33c",
+    "url": "/api-reference/static/js/2.a1963765.chunk.js"
+  },
+  {
+    "revision": "8a615afa3741d9dad911101709fdc8aa",
+    "url": "/api-reference/static/js/2.a1963765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4127299f9ca61f9b3be5",
+    "url": "/api-reference/static/js/3.6b254fbb.chunk.js"
+  },
+  {
+    "revision": "d2ec037cb53c91ad06a9",
+    "url": "/api-reference/static/js/main.4a3dfe8a.chunk.js"
+  },
+  {
+    "revision": "6d8d8e8fcec470bf4ca1",
+    "url": "/api-reference/static/js/runtime-main.a5048bed.js"
+  },
+  {
+    "revision": "2eeb49118c1fbf34e8d2c84e1a00dada",
+    "url": "/api-reference/static/media/business.2eeb4911.yaml"
+  },
+  {
+    "revision": "4df949c0f16988c135a69c5bad5d5e51",
+    "url": "/api-reference/static/media/merchant.4df949c0.yaml"
+  },
+  {
+    "revision": "335728074352f6fb04cbf1654dffe0ef",
+    "url": "/api-reference/static/media/open-banking.33572807.yaml"
+  }
+]);
--- api/service-worker.js
+++ api/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/api-reference/precache-manifest.750403097c1a6434753a7921d23ca019.js"
+  "/api-reference/precache-manifest.ca475370534c07715e97085472d2c0c4.js"
 );
 
 self.addEventListener('message', (event) => {
